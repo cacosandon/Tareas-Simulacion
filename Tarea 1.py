16135739a5b7ea613e8dbedad1374d749ba4e969
Caco Ossandón <jiossandon@uc.cl>
2018-08-10 14:33:52 -0400

    Arreglo V2

--- Tarea 1.py
+++ Tarea 1.py
@@ -4,25 +4,25 @@ from datetime import *
 
 
 class Supermercado:
-    def __init__(self):
+    def __init__(self, cajas):
         self.hora_inicio = datetime(2018, 8, 10, 8, 0, 0)
         self.hora_cierre = datetime(2018, 8, 10, 22, 0, 0)
         self.hora_final = datetime(2018, 8, 11, 1, 0, 0)
         self.hora_actual = datetime(2018, 8, 10, 8, 0, 0)
-        self.colas = [deque() for i in range(16)]
+        self.colas = [deque() for i in range(cajas)]
         #Tiempo de llegada inicial
-        m = expovariate(1/3)
+        m = expovariate(3)
         self.proxima_llegada = self.hora_actual + timedelta(minutes=int(m), seconds=int((m-int(m))*100))
         self.clientes = []
         self.llegadas_cola = [datetime(2018, 8, 11, 1, 0, 0)]
-        self.proximas_atenciones = [datetime(2018,8,11,1,0,0) for i in range (16)]
+        self.proximas_atenciones = [datetime(2018,8,11,1,0,0) for i in range (cajas)]
         self.espera_de_clientes = []
 
     def cola_mas_corta(self):
         mas_corta = [self.colas[0], 0]
         i = 0
         for cola in self.colas:
-            if len(cola) < len(mas_corta):
+            if len(cola) <= len(mas_corta[0]):
                 mas_corta = [cola , i]
             i += 1
 
@@ -33,28 +33,30 @@ class Supermercado:
         while self.hora_actual < self.hora_final:
 
             self.llegadas_cola = [cliente.hora_ingresocola for cliente in self.clientes]+[datetime(2018, 8, 11, 1, 0, 0)]
-            print(self.proxima_llegada, min(self.proximas_atenciones), min(self.llegadas_cola))
-
-
+            
             self.hora_actual = min(self.proxima_llegada, min(self.proximas_atenciones), min(self.llegadas_cola))
             # Despues de trasladarnos al tiempo, vamos al primero que pase.
 
             if self.hora_actual == self.proxima_llegada:
-                if self.proxima_llegada > self.hora_cierre:
+           
+                self.clientes.append(Cliente(self.hora_actual))
+  
+                
+                if self.hora_actual > self.hora_cierre:
+                    
                     self.proxima_llegada = datetime(2018, 8, 11, 1, 0, 0)
                 else:
-                    self.clientes.append(Cliente(self.hora_actual))
-                    m = expovariate(1/3)
+                    m = expovariate(3)
                     self.proxima_llegada = self.hora_actual + timedelta(minutes=int(m), seconds=int((m-int(m))*100))
 
             elif self.hora_actual == min(self.proximas_atenciones):
                 s = 0
                 for caja in self.proximas_atenciones:
                     if caja == self.hora_actual:
-                        cliente_sale = self.colas[s].popleft()
+                        self.colas[s].popleft()
                         if len(self.colas[s]) != 0:
                             self.espera_de_clientes.append((self.hora_actual - self.colas[s][0].hora_ingresocola_estadistica))
-                            h = expovariate(1 / 5)
+                            h = expovariate(1/5)
                             self.proximas_atenciones[s] = self.hora_actual + timedelta(minutes=int(h), seconds=int((h - int(h)) * 100))
                         else:
                             self.proximas_atenciones[s] = datetime(2018,8,11,1,0,0)
@@ -62,18 +64,18 @@ class Supermercado:
                     s += 1
 
             elif self.hora_actual == min(self.llegadas_cola):
-
                 m = 0
                 for llegada_cola in self.llegadas_cola:
+
                     if llegada_cola == self.hora_actual:
-                        cola = self.cola_mas_corta()[0]
+
                         n = self.cola_mas_corta()[1]
                         cliente = self.clientes[m]
-                        cola.append(cliente)
                         cliente.hora_ingresocola_estadistica = cliente.hora_ingresocola
                         cliente.hora_ingresocola = datetime(2018,8,11,1,0,0)
+                        self.colas[n].append(cliente)
 
-                        if len(cola) == 1:
+                        if len(self.colas[n]) == 1:
                             h = expovariate(1/5)
                             self.espera_de_clientes.append(0)
                             self.proximas_atenciones[n] = self.hora_actual + timedelta(minutes=int(h), seconds=int((h-int(h))*100))
@@ -87,7 +89,13 @@ class Supermercado:
                 lista.append(divmod(tiempo.days * 86400 + tiempo.seconds, 60)[0])
             else:
                 lista.append(0)
-        return lista
+        return sum(lista)/len(lista)
+
+    def __str__(self):
+        self.simular()
+        min = int(self.espera_promedio())
+        sec = (self.espera_promedio())%1*(60)
+        return f"El tiempo de espera promedio fue de {min} minutos y {sec} segundos."
 
 
 
@@ -104,12 +112,8 @@ class Cliente:
         
         
        
-prueba = Supermercado()
-prueba.simular()
-h = 0
-for x in prueba.espera_promedio():
-    h += x
-print(h/(len(prueba.espera_promedio())))
+prueba = Supermercado(15)
+print(prueba)
 
 
         
