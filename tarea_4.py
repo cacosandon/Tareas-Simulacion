a6a912c6df3a7a77519ac82a35fa6c0672c888af
Caco Ossandón <jiossandon@uc.cl>
2018-10-23 18:32:35 -0300

    Updating with costs

--- tarea_4.py
+++ tarea_4.py
@@ -11,10 +11,11 @@ class Simulacion:
         self.pedido_actual = 0
         self.costo_total = 0
         self.time = 0
+        self.mes = 0
 
         # Arreglos
         self.proxima_demanda = self.llegada_demanda()
-        self.llegada_orden = 0
+        self.llegada_orden = 500
 
     @staticmethod
     def lead_time():
@@ -26,32 +27,40 @@ class Simulacion:
 
     @staticmethod
     def demanda():
-        return random.choice([1, 2, 3, 4], p=[1/6, 1/3, 1/3, 1/6])
+        return random.choice([1, 2, 3, 4], p=[1 / 6, 1 / 3, 1 / 3, 1 / 6])
 
     def run(self):
         while self.time <= 120:
-
+            # print(self.time, self.proxima_demanda, self.llegada_orden)
             # Si self.time es entero (comienzo de mes)
-            if not (self.time % 1):
+            if self.time // 1 >= self.mes:
+                self.mes += 1
                 if self.inventario < self.s:
                     self.pedido_actual = self.S - self.inventario
-                    self.costo_total += (32 + 3*self.pedido_actual)
+                    self.costo_total += (32 + 3 * self.pedido_actual)
                     self.llegada_orden = self.time + self.lead_time()
             # Veo los tiempos
 
-            if self.llegada_orden > self.proxima_demanda:
+            if self.llegada_orden < self.proxima_demanda:
                 # Sumo el inventario hasta ahora por el costo asociado al tiempo
-                self.costo_total += self.inventario * (self.llegada_orden -
-                                                       self.time)
-                """ Quizas aquí queda negativo de nuevo """
-                self.inventario += (self.pedido_actual - self.pendiente)
+                self.costo_total += (self.inventario + self.pendiente * 5) \
+                                    * (self.llegada_orden - self.time)
+
+                """ Veo si con el pedido quedo en positivo, o sigo debiendo"""
+                if self.inventario + self.pedido_actual - self.pendiente < 0:
+                    self.inventario = 0
+                    self.pendiente = abs(self.inventario + self.pedido_actual
+                                         - self.pendiente)
+                else:
+                    self.inventario += (self.pedido_actual - self.pendiente)
                 self.pendiente = 0
                 self.time = self.llegada_orden
+                self.llegada_orden = 500
             else:
                 demanda = self.demanda()
                 # Sumo el inventario hasta ahora por el costo asociado al tiempo
-                self.costo_total += self.inventario * (self.proxima_demanda -
-                                                       self.time)
+                self.costo_total += (self.inventario + self.pendiente * 5) \
+                                    * (self.proxima_demanda - self.time)
                 if self.inventario - demanda < 0:
                     self.inventario = 0
                     self.pendiente += abs(self.inventario - demanda)
@@ -63,14 +72,9 @@ class Simulacion:
     def __repr__(self):
         return f"Tiempo final: {self.time}\n" \
                f"Costo total: {self.costo_total}\n" \
-               f"Costo promedio: {self.costo_total/120}\n" \
-
+               f"Costo promedio: {self.costo_total/120}\n"
 
 
 simulacion1 = Simulacion(20, 40)
 simulacion1.run()
 print(simulacion1)
-
-
-
-
